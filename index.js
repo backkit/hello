52192bad4d141088cd549aa20f1680d20546dbf7
Dmitri Chapkine <dmitri.chapkine@gmail.com>
2018-11-12 00:21:15 +0100

    switch to awilix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-module.exports = di => {
-  di.service('hello', () => {
-    return (name) => {
-      console.log(`hello ${name}`);
-    }
-  });
-};
+class Hello {
+  hello(name) {
+    console.log(`hello ${name}`);
+  }
+}
+
+module.exports = Hello;
